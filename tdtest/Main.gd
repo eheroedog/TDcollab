780f13ded8446afcec154b3d13cf6307572fb2fc
eheroedog <erickcarreno123@hotmail.com>
2021-08-30 21:11:30 -0500

    tower nav

--- tdtest/Main.gd
+++ tdtest/Main.gd
@@ -12,6 +12,36 @@ func _ready():
 	# towers cannot be placed on these tiles
 	invalid_tiles = $Nav/TileMap_nav.get_used_cells()
 
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and can_place_tower:
+		$tower_placement.clear()
+		
+		# get the tile location of the mouse cursor
+		var tile = $tower_placement.world_to_map(event.position)
+		
+		if not tile in invalid_tiles:
+			# color green / valid tile
+			$tower_placement.set_cell(tile.x, tile.y, 0)
+		else:
+			# color red / invalid tile
+			$tower_placement.set_cell(tile.x, tile.y, 1)
+	elif event is InputEventMouseButton and can_place_tower and event.pressed:
+		# get the tile location of the mouse cursor
+		var tile = $tower_placement.world_to_map(event.position)
+		
+		if not tile in invalid_tiles:
+			can_place_tower = false
+			$tower_placement.clear()
+			
+			# the tile is now invalid for other towers
+			invalid_tiles.append(tile)
+			
+			# place the tower
+			var tower_instance = tower.instance()
+			tower_instance.connect("shoot_projectile", self, "shoot_projectile")
+			tower_instance.position = tile * Vector2(64, 64)
+			$entities.add_child(tower_instance)
+
 func _on_Basic_shoot_projectile(origin, target):
 	var projectile_instance = bullet.instance()
 	projectile_instance.origin_pos = origin
