c1aea75f98b75825d56d85f834d17c9ab8b91dc6
eheroedog <erickcarreno123@hotmail.com>
2021-08-30 20:10:43 -0500

    non blurry

--- tdtest/Main.gd
+++ tdtest/Main.gd
@@ -4,6 +4,14 @@ onready var Enemy = preload("res://enemy1.tscn")
 onready var tower = preload("res://basicTower.tscn")
 onready var bullet = preload("res://bullet.tscn")
 
+var can_place_tower = false
+var invalid_tiles
+
+func _ready():
+	
+	# towers cannot be placed on these tiles
+	invalid_tiles = $Nav/TileMap_nav.get_used_cells()
+
 func _on_Basic_shoot_projectile(origin, target):
 	var projectile_instance = bullet.instance()
 	projectile_instance.origin_pos = origin
